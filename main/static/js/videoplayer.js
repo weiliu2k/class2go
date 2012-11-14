e394d3981cc7e374ee23a60a9239fd770c6932ab
Tiffany Low <tlow@link.(none)>
2012-11-13 16:33:14 -0800

    Added visual indicators for quizzes and subtitles using popcorn.js. Warning: must disable subtitles on youtube.

--- main/static/js/videoplayer.js
+++ main/static/js/videoplayer.js
@@ -35,10 +35,10 @@ function toggleQuizzes(elemId) {
 	console.log("Found", elemId);
 	var elem = document.getElementById(elemId)
 		if (enableQuizzes) {
-			elem.innerHTML = "Show Quizzes";
+			elem.innerHTML = "SQuizzes";
 		}
 		else {
-			elem.innerHTML = "Hide Quizzes";
+			elem.innerHTML = "HQuizzes";
 		}
 	enableQuizzes = !enableQuizzes;
 }
@@ -77,8 +77,12 @@ QuizQn.prototype.onEndFun = function(video, overlayElemId) {
  * 		-- video: Accepts a popcorn video object.
  * 		-- overlayElemId: Id of the overlay element.
  **/
+var isProgressBarMouseDown = false;
+var areCaptionsEnabled = true;
 function VideoPlayer(video, overlayElemId) {
 	this.overlayElemId = overlayElemId;
+	this.markerContainer = "marker-bar";
+	this.cueData;
 	/***
 	 * Add cued events to the overlayElement.
 	 * 		-- cueData is a list of objects with the following fields:
@@ -91,6 +95,7 @@ function VideoPlayer(video, overlayElemId) {
 	 **/
 	this.addCuesToVideo = function(cueData) {
 		if (cueData) {
+			this.cueData = cueData;
 			for (var i = 0; i < cueData.length; i++) {
 				(function(i) {
 					var cueDatum = cueData[i];
@@ -108,6 +113,27 @@ function VideoPlayer(video, overlayElemId) {
 			}
 		}
 	};
+	/***
+	 * Add subtitles to the video:
+	 * 	-NOTE: popcorn.js has a native sbv support which I'm not sure about.
+	 **/
+	this.addSubtitlesToVideo = function(subtitles) {
+		if (subtitles) {
+			for (var i = 0; i < subtitles.length; i++) {
+				(function(i) {
+					var subtitle = subtitles[i];
+					video.text({
+						start: subtitle.start,
+						end: subtitle.end,
+						text: subtitle.caption,
+						multiline: true,
+						escape: true,
+					});
+				})(i);
+			}
+		}
+	};
+
 	this.volume = 1.0; //Set to max volume by default
 	/***
 	 * Adds controls to the video player.
@@ -121,7 +147,7 @@ function VideoPlayer(video, overlayElemId) {
 			var controlElem = document.getElementById(controlElemId);
 
 			//Add callbacks for pause/play controls
-			var playElem = createDiv(controlElemId + "-play", "play");
+			var playElem = createDiv("play", "play");
 			playElem.innerHTML = '<i class="icon-play icon-white"></i>';
 			playElem.addEventListener("click", function() {
 				if (video.paused()) {
@@ -142,25 +168,47 @@ function VideoPlayer(video, overlayElemId) {
 			});
 
 			//Add callback for stop control
-			var stopElem = createDiv(controlElemId + "-stop", "stop");
+			var stopElem = createDiv("stop", "stop");
 			stopElem.innerHTML= '<i class="icon-stop icon-white"></i>';
 			stopElem.addEventListener("click", function() {
 				video.pause(0);
 			},false);
 			controlElem.appendChild(stopElem);
 
+			//Add captioning support (warning: only for HTML5 youtube player)
+			var captionElem = createDiv("caption", "caption");
+			captionElem.innerHTML = 'HCaptions';
+			captionElem.addEventListener("click", function() {
+				if (captionElem) {
+					if (captionElem.style.visibility == "visible") {
+						console.log("hiding captions")
+						captionElem.style.visibility = "hidden";
+						captionElem.innerHTML = 'SCaptions';
+					}
+					else {
+						console.log("showing captions")
+						captionElem.style.visibility = "visible";
+						captionElem.innerHTML = 'HCaptions';
+					}
+				}
+			});
+			controlElem.appendChild(captionElem);
+
 			//Add volume controls
-			var muteElem = createDiv(controlElemId + "-mute", "mute");
+			var muteElem = createDiv("mute", "mute");
 			muteElem.innerHTML = '<i class="icon-volume-up icon-white"></i>';
+			muteElem.muted = false;
 			muteElem.addEventListener("click", function() {
+				console.log(muteElem.muted)
 				if (muteElem.muted) {
 					video.unmute();
-					video.volume(this.volume);
+					//video.volume(this.volume);
+					muteElem.muted = false;
 				}
 				else {
 					video.mute();
+					muteElem.muted = true;
 				}
-			muteElem.muted = !muteElem.muted;
 			}, false);
 
 			video.on("volumechange", function() {
@@ -175,14 +223,14 @@ function VideoPlayer(video, overlayElemId) {
 				}
 			});
 
-			var volumeBoxElem = createDiv(controlElemId + "-volume-box", "volume-box");
-			var volumeSliderElem = createDiv(controlElemId + "-volume-slider", "volume-slider");
+			var volumeBoxElem = createDiv("volume-box", "volume-box");
+			var volumeSliderElem = createDiv("volume-slider", "volume-slider");
 			controlElem.appendChild(volumeBoxElem);
 			volumeBoxElem.appendChild(volumeSliderElem);
 			volumeBoxElem.appendChild(muteElem);
 
 			//Add time display:
-			var timeElem = createDiv(controlElemId + "-time", "time");
+			var timeElem = createDiv("time", "time");
 			timeElem.innerHTML='00:00:00';
 			controlElem.appendChild(timeElem);
 
@@ -201,8 +249,8 @@ function VideoPlayer(video, overlayElemId) {
 			},false);
 
 			//Add progress bar controls
-			var progressBarElemId = controlElemId + "-progress-bar";
-			var timeBarElemId = controlElemId + "-time-bar";
+			var progressBarElemId = "progress-bar";
+			var timeBarElemId = "time-bar";
 			var progressBarElem = createDiv(progressBarElemId, "progress-bar");
 			var timeBarElem = createDiv(timeBarElemId, "time-bar");
 			controlElem.appendChild(progressBarElem);
@@ -223,12 +271,47 @@ function VideoPlayer(video, overlayElemId) {
 				}
 			}, false);
 
-		}
-	};
-}
+
+			var cueContainer = createDiv(this.markerContainer, this.markerContainer);
+			progressBarElem.appendChild(cueContainer);
+			//Add visual indicators for quizzes:
+			if (this.cueData) {
+				var cueData = this.cueData;
+				video.on("loadedmetadata", function() {
+					var maxTime = video.duration();
+					for (var i = 0; i < cueData.length; i++) {
+						(function(i) {
+							var cueDatum = (cueData)[i];
+							var start = cueDatum.start;
+							var cueElemId ="cue"+i;
+							var cueElem = createDiv(cueElemId, "marker");
+							var percentage = 100.0 * start / maxTime;
+							cueElem.style.left = percentage + "%";
+							cueElem.style.z_index = "" + (2+i);
+							cueElem.start = start;
+							cueContainer.appendChild(cueElem);
+
+							var clickListener = (function(elem) {
+								return function(e) {
+									console.log(elem.id + " was pressed ");
+									video.pause(elem.start);
+								}
+							})(cueElem);
+
+							document.getElementById(cueElemId).addEventListener("click", clickListener);
+						})(i);
+					}
+				});
+			}
+		}//if controlElemId
+	} //if controls
+} //VideoPlayer
 
 VideoPlayer.prototype.hideOverlay = function() {
 	tryAccessElem(this.overlayElemId, function(elem) {
+		elem.style.visibility = "hidden";
+	});
+	tryAccessElem(this.markerContainer, function(elem) {
 		elem.style.visibility = "hidden";
 	});
 };
@@ -236,12 +319,14 @@ VideoPlayer.prototype.showOverlay = function() {
 	tryAccessElem(this.overlayElemId, function(elem) {
 		elem.style.visibility = "show";
 	});
+	tryAccessElem(this.markerContainer, function(elem) {
+		elem.style.visibility = "show";
+	});
 };
 
 /***
  * Functions to support seek on the page
  **/
-this.isProgressBarMouseDown = false;
 function seekTo(xPos, progressBarElemId, timeBarElemId,video) {
 	progressBarElemId = "#" + progressBarElemId;
 	var width = $(progressBarElemId).width();
@@ -257,12 +342,7 @@ function seekTo(xPos, progressBarElemId, timeBarElemId,video) {
 	}
 	if (maxTime) {
 		var time = percentage * maxTime / 100;
-		if (video.paused()) {
-			video.pause(time);
-		}
-		else {
-			video.play(time);
-		}
+		video.pause(time);
 		var time = video.currentTime();
 		var percentage = (time / maxTime) * 100.0;
 		document.getElementById(timeBarElemId).style.width = percentage + "%";
