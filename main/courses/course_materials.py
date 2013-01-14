5e144609ee04d135fdb3d1e54d09b5ef8296168b
Jason Bau <jbau@stanford.edu>
2013-01-14 00:38:26 -0800

    merged 2 menu tabs into

--- main/courses/course_materials.py
+++ main/courses/course_materials.py
@@ -21,9 +21,7 @@ def get_course_materials(common_page_data, get_video_content=False, get_pset_con
             exams = Exam.objects.getByCourse(course=COURSE)
         if exam_types:
             exams = exams.filter(exam_type__in=exam_types)
-        
-        print unicode(list(exams))
-                
+                        
         l1items, l2items = get_contentgroup_data(COURSE)
 
         if get_video_content:
